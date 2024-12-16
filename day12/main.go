0619fd535232c4fe5f51ba637c86fb8418eeea07
Dennis Ping <idennisping@gmail.com>
2024-12-15 21:50:25 -0500

    Completed day 12

--- day12/main.go
+++ day12/main.go
@@ -18,9 +18,33 @@ var dirs = [][2]int{
 	{0, -1}, // Left
 }
 
+var cornerPatterns = [][][2]int{
+	{
+		// Top right corner
+		{-1, 0},
+		{0, 1},
+	},
+	{
+		// Bot right corner
+		{1, 0},
+		{0, 1},
+	},
+	{
+		// Bot left corner
+		{1, 0},
+		{0, -1},
+	},
+	{
+		// Top left corner
+		{-1, 0},
+		{0, -1},
+	},
+}
+
 func main() {
 	grid := parseInput("input.txt")
 	part1(grid)
+	part2(grid)
 }
 
 func part1(grid [][]rune) {
@@ -33,7 +57,25 @@ func part1(grid [][]rune) {
 			if !visited[cur] {
 				region := make([]Point, 0)
 				region = floodFillDfs(grid, char, cur, region, visited)
-				total += area(region) * perimeter(grid, region)
+				total += getArea(region) * getPerimeter(grid, region)
+			}
+		}
+	}
+
+	fmt.Println(total)
+}
+
+func part2(grid [][]rune) {
+	total := 0
+	visited := make(map[Point]bool)
+	cur := Point{0, 0}
+	for i, row := range grid {
+		for j, char := range row {
+			cur = Point{i, j}
+			if !visited[cur] {
+				region := make([]Point, 0)
+				region = floodFillDfs(grid, char, cur, region, visited)
+				total += getArea(region) * getSides(grid, region)
 			}
 		}
 	}
@@ -49,7 +91,7 @@ func floodFillDfs(grid [][]rune, char rune, cur Point, region []Point, visited m
 	visited[cur] = true
 	region = append(region, cur)
 
-	neighbors, _ := getNeighbors(grid, cur)
+	neighbors := getNeighbors(grid, cur)
 	for _, nb := range neighbors {
 		region = floodFillDfs(grid, char, nb, region, visited)
 	}
@@ -57,40 +99,103 @@ func floodFillDfs(grid [][]rune, char rune, cur Point, region []Point, visited m
 	return region
 }
 
-func getNeighbors(grid [][]rune, cur Point) ([]Point, []Point) {
-	var neighbors, oobs []Point // oob = out of bounds
+func getNeighbors(grid [][]rune, cur Point) []Point {
+	var neighbors []Point // oob = out of bounds
 	for _, dir := range dirs {
-		nextR := cur.R + dir[0]
-		nextC := cur.C + dir[1]
-		if inBounds(nextR, nextC, len(grid), len(grid[0])) {
-			neighbors = append(neighbors, Point{nextR, nextC})
-		} else {
-			oobs = append(oobs, Point{nextR, nextC})
+		nr := cur.R + dir[0]
+		nc := cur.C + dir[1]
+		if inBounds(nr, nc, len(grid), len(grid[0])) {
+			neighbors = append(neighbors, Point{nr, nc})
 		}
 	}
 
-	return neighbors, oobs
+	return neighbors
+}
+
+func getOutOfBounds(grid [][]rune, cur Point) []Point {
+	var oobs []Point // oob = out of bounds
+	for _, dir := range dirs {
+		nr := cur.R + dir[0]
+		nc := cur.C + dir[1]
+		if !inBounds(nr, nc, len(grid), len(grid[0])) {
+			oobs = append(oobs, Point{nr, nc})
+		}
+	}
+
+	return oobs
 }
 
 func inBounds(r, c, n, m int) bool {
 	return r >= 0 && r < n && c >= 0 && c < m
 }
 
-func area(region []Point) int {
+func getArea(region []Point) int {
 	return len(region)
 }
 
-func perimeter(grid [][]rune, region []Point) int {
-	count := 0
+func getPerimeter(grid [][]rune, region []Point) int {
+	perim := 0
 	for _, point := range region {
-		neighbors, oobs := getNeighbors(grid, point)
+		neighbors := getNeighbors(grid, point)
 		for _, nb := range neighbors {
 			if grid[point.R][point.C] != grid[nb.R][nb.C] {
-				count++
+				perim++
 			}
 		}
-		count += len(oobs)
+		oobs := getOutOfBounds(grid, point)
+		perim += len(oobs)
 	}
+
+	return perim
+}
+
+func getSides(grid [][]rune, region []Point) int {
+	sides := 0
+	for _, point := range region {
+		count := countCorners(grid, point)
+		sides += count
+	}
+
+	return sides
+}
+
+func countCorners(grid [][]rune, cur Point) int {
+	char := grid[cur.R][cur.C]
+	count := 0
+
+	for _, pattern := range cornerPatterns {
+		// Directional offsets
+		dirA := pattern[0]
+		dirB := pattern[1]
+
+		// New (row, col)
+		rA, cA := cur.R+dirA[0], cur.C+dirA[1]
+		rB, cB := cur.R+dirB[0], cur.C+dirB[1]
+		rD, cD := rA+dirB[0], cA+dirB[1] // Diagonal cell
+
+		insideA := inBounds(rA, cA, len(grid), len(grid[0])) && grid[rA][cA] == char
+		insideB := inBounds(rB, cB, len(grid), len(grid[0])) && grid[rB][cB] == char
+		insideD := inBounds(rD, cD, len(grid), len(grid[0])) && grid[rD][cD] == char
+
+		// A cell is a boundary if it's out-of-bounds or different char
+		boundaryA := !insideA
+		boundaryB := !insideB
+		boundaryD := !insideD
+
+		// Convex corner condition:
+		// Both A and B are boundaries
+		if boundaryA && boundaryB {
+			count++
+			continue
+		}
+
+		// Concave corner condition:
+		// A and B are inside, but the diagonal is a boundary
+		if insideA && insideB && boundaryD {
+			count++
+		}
+	}
+
 	return count
 }
 
